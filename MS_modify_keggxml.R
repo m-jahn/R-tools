758cc9db489878b104fbd9f91d43f0ac814f511c
Michael Jahn <michael.jahn@scilifelab.se>
2019-01-18 23:04:15 +0100

    improved parsing of Kegg file

--- MS_modify_keggxml.R
+++ MS_modify_keggxml.R
@@ -1,15 +1,27 @@
-## DATA HANDLING
+#
+# This script is a small utility to parse Kegg Brite XML files and return
+# a regular data frame instead. The data frane can be merged with gene 
+# expression data and be used to cluster such data based on Kegg Brite
+# categories
+# 
+# Author: Michael Jahn, PhD
+# Affiliation: Scilifelab (KTH), Stockholm
+# Date: 2019-01-17
+#
+# 
+# LIBRARIES FOR DATA MANIPULATION ++++++++++++++++++++++++++++++++++++++++++++++
 library(tidyr)
 library(plyr)
-#rm(list=ls())
-setwd("Documents/SciLifeLab/Resources/MS/databases/")
 
-# in Kegg text file: 
-# replace all commas by semicolon or minus (e.g. in fructose-1,6-bisphosphate), 
-# replace double space >  < by comma >,<
-# replace tab >\t< by comma >,<
 
-brite	<- read.table("KeggBrite.txt", sep=",", fill=TRUE, stringsAsFactors=FALSE, quote = "\"")
+# CHANGES THAT NEED TO BE MADE TO THE KEGG XML FILE
+# in Kegg text file, replace double spaces >  < by comma >,< or >\t<
+# Remove first lines until regular content begins
+# Possibly add some trailing tabs or commas to end of first line
+# so that read.table knows how many columns to expect
+
+brite	<- read.table("KEGG_Brite_20180910_reh00001.keg", sep="\t", 
+  fill=TRUE, stringsAsFactors=FALSE, quote = "\"")
 colnames(brite) <-	c("System","Process","Pathway","Protein", "EC-class")
 
 # purge the table from tags and sybmbols
@@ -27,13 +39,23 @@ brite[1:3] <- as.data.frame(
   })
 )
 
-# split protein column in ID and trivial name
-brite$GeneID <- substr(brite$Protein, 1, 7)
-brite$Protein <- substr(brite$Protein, 8, 200)
+# split protein column in IDs and trivial name
+brite <- separate(brite, Protein, c("Protein", "full_name"), sep="; ")
+brite <- separate(brite, Protein, c("Protein", "short_name"), sep=" ")
 
 # remove rows without proteins or exp values
-brite <- subset(brite, GeneID != "")
+brite <- subset(brite, Protein != "")
+
+# trim some filling material to more concise description
+brite <- apply(brite, 2, function(x) {
+  gsub("A[0-9]{5} (Brite Hierarchies)?|Protein families: | - prokaryotes", "", x)
+}) %>% as.data.frame
+
+# optionally propagate some terms from second to first level if missing
+brite[brite$Process=="genetic information processing", "System"] <- "Genetic Information Processing"
+brite[brite$Process=="metabolism", "System"] <- "Metabolism"
+brite[brite$Process=="signaling and cellular processes", "System"] <- "Cellular Processes"
 
 
 # export re-formatted csv table
-write.csv(brite, file="KeggBrite.csv")
+write.csv(brite, file="KEGG_Brite_20180910_reh00001.csv")
