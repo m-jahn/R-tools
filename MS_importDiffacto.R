5da6aa1aabd331aec2d09f3be31f32233fdd295f
Michael Jahn <michael.jahn@scilifelab.se>
2019-05-20 14:38:36 +0200

    updated normalization algorithms, tidy syntax

--- MS_importDiffacto.R
+++ MS_importDiffacto.R
@@ -23,21 +23,21 @@ source("custom.theme.R")
 #
 # load raw data
 df <- read.table("/media/ProteomicsCyano/MS_analysis/20181204/Diffacto/peptides_DeMixQ_Diffacto.tsv",
-  header=TRUE, stringsAsFactors=FALSE, sep="\t")
+  header = TRUE, stringsAsFactors = FALSE, sep = "\t")
 # define value columns
 sample.cols <- 4:ncol(df)
 # replace '1' (DemixQ output for not-quantified) with NA
-df[ , sample.cols] <- apply(df[sample.cols], 2, function(x) replace(x, x==1, NA))
+df[ , sample.cols] <- apply(df[sample.cols], 2, function(x) replace(x, x == 1, NA))
 # define normalization function (one of "none", "normalizeMedianValues",
 # "normalize.quantiles", "normalize.quantiles.robust", 
 # "normalize.quantiles.in.blocks", "justvsn")
 norm.function <- "justvsn"
 # flag for plot controls
-plot.norm <- TRUE; plot.to.png=TRUE
+plot.norm <- TRUE; plot.to.png = TRUE
 # aggregation method, one of ("sum", "weightedsum", "mean", "weightedmean", "wgeomean")
 method <- "weightedsum"
 # filename prefix
-filename="diffacto_vsnNorm_"
+filename = "diffacto_vsnNorm_"
 # define the output directory(s) where tables are saved
 output_dir <- list(
   "/media/ProteomicsCyano/MS_analysis/20181204/Diffacto/quantification/",
@@ -64,43 +64,54 @@ output_dir <- list(
 # The ranks replace intensities so that differences _within one sample_ 
 # are distorted
 
-apply.norm <- function(df, norm.function, sample.cols) {
+apply.norm <- function(
+  df, norm.function, sample.cols = 1:ncol(df),
+  ref.cols = NULL) {
   
-  if (norm.function=="normalize.quantiles.in.blocks") {
+  if (norm.function == "normalize.quantiles.in.blocks") {
+    
     # this function requires blocks. We define an arbitrary number of blocks
     # from the sorted median intensity of all samples
-    blocks <- apply(df[sample.cols], 1, FUN=function(x) median(x, na.rm=TRUE)) %>% 
-      quantcut(q=20)
-    df[1:nrow(df), sample.cols] <- 
-    as.matrix(df[sample.cols]) %>%
-    normalize.quantiles.in.blocks(., blocks) %>%
-    as.data.frame
-    df
-  }
-  else {
-    df[1:nrow(df), sample.cols] <- 
-    do.call(
-      get(norm.function), 
-      list(x=as.matrix(df[sample.cols]))
-    ) %>%
-    as.data.frame
-    if (norm.function=="justvsn") {
-      df[sample.cols] <- apply(df[sample.cols], 2, function(x) 2^x)
+    blocks <- apply(df[sample.cols], 1, function(x) median(x, na.rm = TRUE)) %>% 
+      quantcut(q = 20)
+    df_norm <- df[sample.cols] %>% as.matrix %>%
+      normalize.quantiles.in.blocks(blocks)
+    
+  } else if (norm.function == "justvsn") {
+      
+    if (is.null(ref.cols)) {
+      df_norm <- justvsn(as.matrix(df[sample.cols])) %>% 
+        apply(2, function(x) 2^x)
+    } else {
+      df_norm <- justvsn(as.matrix(df[sample.cols]), 
+        reference = vsn2(as.matrix(df[ref.cols]))) %>% 
+        apply(2, function(x) 2^x)
     }
-    df
+      
+  } else if (norm.function != "none") {
+      
+    df_norm <- do.call(
+      get(norm.function), 
+      list(x = as.matrix(df[sample.cols]))
+    )
+      
+  } else {
+    df_norm <- df
   }
+  # return normalized dataframe
+  df_norm <- as.data.frame(df_norm) %>% setNames(colnames(df[sample.cols]))
+  bind_cols(df %>% select(-sample.cols), df_norm)
 }
-
 
 # FUNCTION TO AVERAGE PEPTIDES +++++++++++++++++++++++++++++++++++++++++++++++++
 #
 # define a set of different quantification methods
 methods <- list(
-  sum=function(x, weight) sum(x, na.rm=TRUE),
-  weightedsum=function(x, weight) sum(x*weight, na.rm=TRUE),
-  mean=function(x, weight) mean(x, na.rm=TRUE),
-  weightedmean=function(x, weight) weighted.mean(x, weight, na.rm=TRUE),
-  wgeomean=function(x, weight) exp(weighted.mean(log(x), weight, na.rm=TRUE))
+  sum = function(x, weight) sum(x, na.rm = TRUE),
+  weightedsum = function(x, weight) sum(x*weight, na.rm = TRUE),
+  mean = function(x, weight) mean(x, na.rm = TRUE),
+  weightedmean = function(x, weight) weighted.mean(x, weight, na.rm = TRUE),
+  wgeomean = function(x, weight) exp(weighted.mean(log(x), weight, na.rm = TRUE))
 )
 
 # function to aggregate peptide abundances and collect in new df
@@ -116,13 +127,13 @@ aggregate.pep <- function(data, sample.cols, weight.threshold, method) {
       # case 2: some peptides are lower than threshold
       else if (any(subdf$weight >= weight.threshold) & 
         !all(subdf$weight >= weight.threshold)) {
-        cat("filtering by weight: ", sum(subdf$weight>=weight.threshold),
+        cat("filtering by weight: ", sum(subdf$weight >= weight.threshold),
           " out of ", nrow(subdf), "accepted \n")
-        subdf <- subset(subdf, weight>=weight.threshold)
+        subdf <- subset(subdf, weight >= weight.threshold)
       }
       else cat("protein ", unique(subdf$protein), " has no valid peptide \n")
       # apply aggregation method per protein, optional weight argument
-      apply(subdf[sample.cols], 2, method, weight=subdf$weight)
+      apply(subdf[sample.cols], 2, method, weight = subdf$weight)
     })
   )
 }
@@ -142,23 +153,23 @@ plot.function <- function(df, df.norm) {
   # combine original and normalized (reduced) data frame
   rbind(df[sam, ], df.norm[sam, ]) %>% 
   # add serial number and group (original vs normalized)
-  mutate(., number=rep(1:length(sam),2)) %>%
-  mutate(., normalized=rep(c("not normalized", "normalized"), each=length(sam))) %>%
+  mutate(., number = rep(1:length(sam),2)) %>%
+  mutate(., normalized = rep(c("not normalized", "normalized"), each = length(sam))) %>%
     
   # gather to long format
   gather(., sample, intensity, -peptide, -protein, -weight, -number, -normalized) %>%
     
   # and plot using lattice
   xyplot(log10(intensity) ~ number | sample, .,
-    par.settings=custom.lattice, type="l",
-    as.table=TRUE,
-    groups=normalized, auto.key=list(columns=2, lines=TRUE, points=FALSE),
-    panel=function(x, y, ...) {
-      panel.grid(h=-1, v=-1, col=grey(0.9))
+    par.settings = custom.lattice, type = "l",
+    as.table = TRUE,
+    groups = normalized, auto.key = list(columns = 2, lines = TRUE, points = FALSE),
+    panel = function(x, y, ...) {
+      panel.grid(h = -1, v = -1, col = grey(0.9))
       panel.superpose(x, y, ...)
     },
-    panel.groups=function(x, y, ...) {
-      panel.xyplot(x, sort(y, na.last=TRUE), ...)
+    panel.groups = function(x, y, ...) {
+      panel.xyplot(x, sort(y, na.last = TRUE), ...)
     }
   )
 }
@@ -167,7 +178,7 @@ plot.function <- function(df, df.norm) {
 # MAIN +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #
 # apply optional normalization function
-if (norm.function!="none") {
+if (norm.function != "none") {
   df.norm <- apply.norm(df, norm.function, sample.cols)
 }
 
@@ -177,7 +188,7 @@ if (plot.norm & norm.function != "none") {
   if (plot.to.png) {
     for (out in output_dir) {
       fullfilename <- paste0(filename, method, ".png")
-      png(paste0(out, fullfilename), width=1000, height=1000, res=110)
+      png(paste0(out, fullfilename), width = 1000, height = 1000, res = 110)
       print(normplot)
       cat("writing", paste0(out, fullfilename), "...\n")
       dev.off()
@@ -189,10 +200,10 @@ if (plot.norm & norm.function != "none") {
 # apply aggregation with parameters for method, samples, 
 # weight cutoff (Diffacto's default is 0.5)
 df.result <- aggregate.pep(
-  data = {if (norm.function=="none") df else df.norm}, 
+  data = {if (norm.function == "none") df else df.norm}, 
   sample.cols, 
-  weight.threshold=0.5, 
-  method=methods[[method]]
+  weight.threshold = 0.5, 
+  method = methods[[method]]
 )
   
 # change column name
@@ -201,12 +212,12 @@ colnames(df.result)[1] <- "protein"
 # remove ambiguous entries
 df.result <- subset(df.result, !grepl("CONT_|XXX_", protein))
 # and replace (near-) zeros with NA
-df.result[-1] <- apply(df.result[-1], 2, function(x) replace(x, x<=10, NA))
+df.result[-1] <- apply(df.result[-1], 2, function(x) replace(x, x <= 10, NA))
 
 # save results as CSV file
 fullfilename <- paste0(filename, method, ".csv")
 
 for (out in output_dir) {
   cat("writing", paste0(out, fullfilename), "...\n")
-  write.csv(df.result, file=paste0(out, fullfilename))
+  write.csv(df.result, file = paste0(out, fullfilename))
 }
