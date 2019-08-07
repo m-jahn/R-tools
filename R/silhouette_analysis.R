b8fbe608627abd46f09c74a6c649da461030660b
Michael Jahn <michael.jahn@scilifelab.se>
2019-08-07 13:22:06 +0200

    silhouette wrapper now also supports kmeans clustering, not only hclust

--- R/silhouette_analysis.R
+++ R/silhouette_analysis.R
@@ -1,38 +1,44 @@
-#' Wrapper function to perform silhouette analysis on all clusters of a hclust object
+#' Wrapper function to perform silhouette analysis on different cluster numbers
 #' 
 #' Silhouette analysis shows the clusters that have explanatory power.
-#' That includes clusters that are best separated from the neighbours (positive score)
-#'
-#' this function applies the iterative silhouette analysis
-#' and stores results in a list
+#' That includes clusters that are best separated from the neighbours 
+#' resulting in a higher average silhoutte width (the decisive metric to 
+#' judge optimal cluster number). This function applies the silhouette analysis iteratively for a vector of 
+#' different cluster numbers and stores results in a list.
 #' 
 #' @importFrom cluster silhouette
 #' @importFrom dendextend is.hclust
 #' @importFrom plyr ldply
 #' @importFrom lattice xyplot
 #' 
-#' @param mat (numeric matrix) the original data matrix that clustering was performed on
-#' @param cluster_object (hclust) the cluster object obtained from running hclust()
+#' @param mat (numeric matrix) data matrix that clustering was performed on 
+#'   (or will be performed using k-means clustering)
+#' @param cluster_object (hclust) a cluster object obtained from running hclust(), optional
 #' @param n_clusters (numeric) a vector of cluster numbers for which silhouette analysis is
 #'   performed
 #' 
 #' @details Prerequesite for silhouette analysis is a cluster object that can be obtained
-#' by e.g. running hclust(d = dist(mat), method = "ward.D").
+#'   by e.g. running hclust(d = dist(mat), method = "ward.D"). The alternative is to supply
+#'   no cluster object, then the functions performs a kmeans() clustering for the indicated
+#'   number of clusters.
 #' 
-#' @return a list with three objects, silhouette analysis data, and two lattice result plots
+#' @return a list with three objects, silhouette analysis data, and two summary plots
+#'   obtained using lattice graphics.
 #' 
 #' @export
 # ------------------------------------------------------------------------------
 silhouetteAnalysis <- function(
   mat, 
-  cluster_object, 
+  cluster_object = NULL,
   n_clusters
 ) {
   
-  # check object properties
-  if (dendextend::is.hclust(cluster_object)) 
-    print(cluster_object)
-  else stop("cluster_object is not a hclust object\n")
+  # check cluster_object properties
+  if (!is.null(cluster_object)) {
+    if (dendextend::is.hclust(cluster_object)) 
+      print(cluster_object)
+    else stop("cluster_object that was provided is not of class hclust\n")
+  }
   
   if (!is.numeric(n_clusters))
     stop("n_clusters is not a numeric vector\n")
@@ -42,10 +48,19 @@ silhouetteAnalysis <- function(
   # loop through silhouette function
   dat <- lapply(n_clusters, function(x) {
     
-    si.summary <- silhouette(
-      as.numeric(cutree(cluster_object, k = x)),
-      dist(mat)
-    ) %>% summary
+    # if no cluster_object is provided use kmeans clustering
+    if (is.null(cluster_object)) {
+      kmeans_cluster <- kmeans(mat, centers = x, iter.max = 100)$cluster
+      si.summary <- silhouette(as.numeric(kmeans_cluster), dist(mat)) %>% summary
+    
+    # else use the provided hclust object
+    } else {
+      si.summary <- silhouette(
+        as.numeric(cutree(cluster_object, k = x)),
+        dist(mat)
+      ) %>% summary
+    }
+    
     si.summary$n_clusters <- rep(x, x)
     si.summary[c("n_clusters", 
       "clus.avg.widths", 
@@ -59,7 +74,7 @@ silhouetteAnalysis <- function(
   #as.numeric(clus.sizes.Freq)
   plot.clusters <- xyplot(clus.avg.widths ~ as.numeric(clus.sizes.cl)
       | factor(n_clusters), dat,
-    par.settings = custom.lattice, as.table = TRUE,
+    par.settings = custom.lattice, as.table = TRUE, border = FALSE,
     xlab = "cluster elements", ylab = "silhouette width",
     panel = function(x, y, ...) {
       panel.grid(h = -1, v = -1, col = grey(0.9))
