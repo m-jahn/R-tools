b59cb3947c7e78c775c4c0165682b6deb949c658
Michael Jahn <michael.jahn@scilifelab.se>
2019-09-10 11:43:41 +0200

    updated documentation

--- R/parse_kegg_brite.R
+++ R/parse_kegg_brite.R
@@ -7,11 +7,16 @@
 #' 
 #' @param data a data frame with 5 columns as input
 #' 
-#' @details Changes that need to be made to the Kegg XML file before applying function (!):
-#' - replace double spaces \code{'  '} by commas \code{','} or tabs \code{'\t'}
+#' @details Changes that need to be made to the Kegg XML file before applying the function, simply using a text editor:
+#' 
+#' - replace double spaces \code{'  '} by tabs \code{'\t'}
 #' - remove first lines until regular content begins
-#' - possibly add some trailing tabs or commas to end of first line
+#' - possibly add some trailing tabs or commas to end of first line (4 to 5)
 #'   so that read.table knows how many columns to expect
+#' - read raw data frame into R using
+#'   \code{read.table("/path/to/file", fill = TRUE, sep = "\t", row.names = NULL, 
+#'     stringsAsFactors = FALSE, quote = "")}
+#' 
 #'   
 #' @export
 # ------------------------------------------------------------------------------
@@ -39,8 +44,8 @@ parse_kegg_brite <- function(data) {
   )
   
   # split protein column in IDs and trivial name
-  data <- tidyr::separate(data, Protein, c("Protein", "full_name"), sep="; ")
-  data <- tidyr::separate(data, Protein, c("Protein", "short_name"), sep=" ")
+  data <- tidyr::separate(data, Protein, c("Protein", "full_name"), sep="; ") %>%
+    tidyr::separate(data, Protein, c("Protein", "short_name"), sep=" ")
   
   # remove rows without proteins or exp values
   data <- subset(data, Protein != "")
